ec875062a48023b8c0257cc7135d19c21eaee2d1
Hjn4 <kaycibana11032003@gmail.com>
2023-11-03 16:36:26 +0700

    to clear format file name, auto create script

--- auto_create_folder.py
+++ auto_create_folder.py
@@ -7,30 +7,33 @@ import os
 import subprocess
 
 folder_name = input("Input your folder name: ")
-number_of_file = int(input("Input your number files: "))
+number_of_files = int(input("Input your number files: "))
 template_file_name = input(
     "Input your template (when X will be replaced like baiX.py => bai123.py): ")
 
 solution = int(
     input("Input your choose 1 or 2 for difference solution but just one result: "))
 
+num_digits = len(str(number_of_files))
 
 match solution:
     case 1:
         if not os.path.exists(folder_name):
             os.mkdir(folder_name)
 
-        for i in range(1, number_of_file + 1):
-            file_name = template_file_name.replace("X", str(i))
+        for i in range(1, number_of_files + 1):
+            formatted_i = f"{i:0{num_digits}}"
+            file_name = template_file_name.replace("X", formatted_i)
             with open(os.path.join(folder_name, file_name), "w") as file:
                 file.write(f"# file number: {i}")
-        print(f"Created {number_of_file} files in folder {folder_name}")
+        print(f"Created {number_of_files} files in folder {folder_name}")
 
     case _:
         subprocess.run(["mkdir", folder_name])
 
-        for i in range(1, number_of_file + 1):
-            file_name = template_file_name.replace("X", str(i))
+        for i in range(1, number_of_files + 1):
+            formatted_i = f"{i:0{num_digits}}"
+            file_name = template_file_name.replace("X", formatted_i)
             subprocess(["touch", os.path.join(folder_name, file_name)])
 
-        print(f"Created {number_of_file} files in folder {folder_name}")
+        print(f"Created {number_of_files} files in folder {folder_name}")
