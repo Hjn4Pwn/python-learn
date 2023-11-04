a030d2d2b6bbd4c8b58019b500367d14adf15bd7
Hjn4 <kaycibana11032003@gmail.com>
2023-11-05 00:02:21 +0700

    update

--- level2/bai_10.py
+++ level2/bai_10.py
@@ -1,1 +1,16 @@
-# file number: 10
+'''
+Viết một chương trình tính giá trị của a+aa+aaa+aaaa với a là số được nhập vào bởi người dùng.
+
+Giả sử a được nhập vào là 1 thì đầu ra sẽ là: 1234
+
+'''
+
+def cal(n):
+    a = int("%s" % (n))
+    aa = int("%s%s" % (n, n))
+    aaa = int("%s%s%s" % (n, n, n))
+    aaaa = int("%s%s%s%s" % (n, n, n, n))
+    return a + aa + aaa + aaaa
+
+
+print(cal(int(input("Input your number: "))))
