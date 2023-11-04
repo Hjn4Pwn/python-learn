1a1263cd34f386f87277677a2c29cdcf612a3df6
Hjn4 <kaycibana11032003@gmail.com>
2023-11-05 00:02:02 +0700

    update

--- level2/bai_06.py
+++ level2/bai_06.py
@@ -1,1 +1,15 @@
-# file number: 6
+'''
+Viết một chương trình chấp nhận đầu vào là chuỗi các số nhị phân 4 chữ số, 
+phân tách bởi dấu phẩy, kiểm tra xem chúng có chia hết cho 5 không. 
+Sau đó in các số chia hết cho 5 thành dãy phân tách bởi dấu phẩy.
+
+Ví dụ đầu vào là: 0100,0011,1010,1001
+Đầu ra sẽ là: 1010
+'''
+
+result = []
+for i in input("Input your list of numbers: ").split(","):
+    convert_str_to_bin = int(i, 2)
+    if convert_str_to_bin % 5 == 0:
+        result.append(i)
+print(", ".join(result))
