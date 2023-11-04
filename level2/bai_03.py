ee76b123190f98449945166e2707d85b9258d68c
Hjn4 <kaycibana11032003@gmail.com>
2023-11-04 09:00:14 +0700

    update

--- level2/bai_03.py
+++ level2/bai_03.py
@@ -1,1 +1,16 @@
-# file number: 3
+'''
+Viết một chương trình chấp nhận chuỗi từ do người dùng nhập vào, phân tách nhau bởi dấu phẩy 
+và in những từ đó thành chuỗi theo thứ tự bảng chữ cái, phân tách nhau bằng dấu phẩy.
+Giả sử đầu vào được nhập là: without,hello,bag,world, thì đầu ra sẽ là: bag,hello,without,world.
+
+'''
+
+# input_str = input("Input your list word: ")
+
+# arr = input_str.split(",")
+# arr.sort()
+# print(arr)
+
+arr = [x for x in input("Input your string: ").split(",")]
+arr.sort()
+print(arr)
