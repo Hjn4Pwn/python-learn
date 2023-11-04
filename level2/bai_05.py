b0cd9502870f92c3f0dfd97b1ba18ff4e71b2329
Hjn4 <kaycibana11032003@gmail.com>
2023-11-05 00:00:23 +0700

    update

--- level2/bai_05.py
+++ level2/bai_05.py
@@ -1,1 +1,11 @@
-# file number: 5
+'''
+Viết một chương trình chấp nhận đầu vào là một chuỗi các từ tách biệt bởi khoảng trắng, 
+loại bỏ các từ trùng lặp, sắp xếp theo thứ tự bảng chữ cái, rồi in chúng.
+
+Giả sử đầu vào là: hello world and practice makes perfect and hello world again
+Thì đầu ra là: again and hello makes perfect practice world
+
+'''
+
+set_arr = set([x for x in input("Input your strings: ").split(" ")])
+print(" ".join(sorted(set_arr)))
